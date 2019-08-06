f38df058ec5d4048743f3fd077941b886e489350
Guiming Chen <42054519+YidaoXianren@users.noreply.github.com>
2019-08-06 01:12:16 -0400

    Add files via upload

---
+++ helloDemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+    Author: Yidao Chen
+    Data: 08/06/2019
+ */
+
+//This is a main function
+int main()
+{
+    printf("Hello World!!!"); //haha, I can also leave comment here.
+    return 0;
+}
